ee3c9b189bb5e33ac5b9d9b04cdbb98db5c8b78c
Santiago Reyes Buitrago <santiagoreyesbuitrago02@gmail.com>
2024-07-22 14:37:11 -0500

    uso de la variables de entorno

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from decouple import config
 
-URL_DATABASE = "mysql+pymysql://root:secret@localhost:3306/CONCESIONARIO"
+URL_DATABASE = f"mysql+pymysql://{config('MYSQL_USER')}:{config('MYSQL_PASSWORD')}@{config('MYSQL_HOST')}:{config('MYSQL_PORT')}/{config('MYSQL_BD')}"
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Bd = declarative_base()
