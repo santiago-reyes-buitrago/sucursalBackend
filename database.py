d01b89ec9ebf399be958312081ed42fe59915ad5
Santiago Reyes Buitrago <santiagoreyesbuitrago02@gmail.com>
2024-07-22 17:08:44 -0500

    Reconfiguracion de los cors - prueba local

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from decouple import config
 
 URL_DATABASE = f"mysql+pymysql://{config('MYSQL_USER')}:{config('MYSQL_PASSWORD')}@{config('MYSQL_HOST')}:{config('MYSQL_PORT')}/{config('MYSQL_BD')}"
+print(URL_DATABASE)
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Bd = declarative_base()
